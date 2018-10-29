9231023019eab60a00d68b325da260bd9f4b01a8
sev7e0 <sev7e0@163.com>
2018-10-29 23:50:08 +0800

    :dog: 删除未用的包

--- src/main/java/com/sev7e0/MR/duplicate/DuplicateRemoval.java
+++ src/main/java/com/sev7e0/MR/duplicate/DuplicateRemoval.java
@@ -1,17 +1,14 @@
 package com.sev7e0.MR.duplicate;
 
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
